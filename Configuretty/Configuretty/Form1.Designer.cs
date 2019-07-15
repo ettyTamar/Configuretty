9eb1d1bd2ff7f89a7b42d545c2b8d1ef3407301b
etty tamar <etty.ohayon@gmail.com>
2019-07-15 20:20:40 +0300

    Commit_one

---
+++ Configuretty/Configuretty/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Configuretty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BattleDimension = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Country = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Function = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.LINK = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DIS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Load = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gainsboro;
+            this.label1.Font = new System.Drawing.Font("Californian FB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(90, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DIS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(232, 73);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = " ";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Font = new System.Drawing.Font("Californian FB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(90, 118);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(232, 115);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // BattleDimension
+            // 
+            this.BattleDimension.AutoSize = true;
+            this.BattleDimension.BackColor = System.Drawing.Color.Gainsboro;
+            this.BattleDimension.Font = new System.Drawing.Font("Californian FB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BattleDimension.ForeColor = System.Drawing.Color.Black;
+            this.BattleDimension.Location = new System.Drawing.Point(90, 159);
+            this.BattleDimension.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BattleDimension.Name = "BattleDimension";
+            this.BattleDimension.Size = new System.Drawing.Size(101, 15);
+            this.BattleDimension.TabIndex = 4;
+            this.BattleDimension.Text = "BattleDimension";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "F",
+            "G",
+            "S"});
+            this.comboBox1.Location = new System.Drawing.Point(232, 159);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 23);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Country
+            // 
+            this.Country.AutoSize = true;
+            this.Country.BackColor = System.Drawing.Color.Gainsboro;
+            this.Country.Font = new System.Drawing.Font("Californian FB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Country.ForeColor = System.Drawing.Color.Black;
+            this.Country.Location = new System.Drawing.Point(90, 205);
+            this.Country.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(54, 15);
+            this.Country.TabIndex = 6;
+            this.Country.Text = "Country";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(232, 196);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(187, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged_1);
+            // 
+            // Function
+            // 
+            this.Function.AutoSize = true;
+            this.Function.BackColor = System.Drawing.Color.Gainsboro;
+            this.Function.Font = new System.Drawing.Font("Californian FB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Function.ForeColor = System.Drawing.Color.Black;
+            this.Function.Location = new System.Drawing.Point(90, 250);
+            this.Function.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Function.Name = "Function";
+            this.Function.Size = new System.Drawing.Size(57, 15);
+            this.Function.TabIndex = 8;
+            this.Function.Text = "Function";
+            this.Function.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(232, 241);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(187, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // LINK
+            // 
+            this.LINK.BackColor = System.Drawing.Color.DarkGray;
+            this.LINK.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.LINK.Font = new System.Drawing.Font("Californian FB", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LINK.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LINK.Location = new System.Drawing.Point(252, 343);
+            this.LINK.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.LINK.Name = "LINK";
+            this.LINK.Size = new System.Drawing.Size(100, 27);
+            this.LINK.TabIndex = 10;
+            this.LINK.Text = "LINK";
+            this.LINK.UseVisualStyleBackColor = false;
+            this.LINK.Click += new System.EventHandler(this.LINK_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.DIS});
+            this.dataGridView1.Location = new System.Drawing.Point(586, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            // 
+            // DIS
+            // 
+            this.DIS.HeaderText = "DIS";
+            this.DIS.Name = "DIS";
+            this.DIS.ReadOnly = true;
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(586, 246);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 16;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1067, 520);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.LINK);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Function);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Country);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.BattleDimension);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Californian FB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Configuretty";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label BattleDimension;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Country;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Function;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button LINK;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DIS;
+        private System.Windows.Forms.Button Load;
+    }
+}
+
---
+++ Configuretty/Configuretty/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.Win32;
+
+namespace Configuretty
+{
+    public partial class Form1 : Form
+    {
+        string m_filepath = "";
+        XmlDocument xdoc = new XmlDocument();
+        List<string> namesList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> GetNamesFromXml(string filepath)
+        {
+            xdoc.Load(m_filepath);
+
+            List<string> namesList = new List<string>();
+
+            foreach (XmlNode node in xdoc.SelectNodes("//Mappings/Descriptions/Description/Name"))
+            {
+                string name = node.InnerText;
+
+                namesList.Add(name);
+            }
+
+            return namesList.Distinct().ToList();
+        }
+
+        private void LINK_Click(object sender, EventArgs e)
+        {
+            string regex = @"^\d{1,9}[':']\d{1,9}[':']\d{1,9}[':']\d{1,9}[':']\d{1,9}[':']\d{1,9}[':']\d{1,9}";
+            string regex2 = @"^[A-Z]{1,2}";
+
+            if (!Regex.IsMatch(textBox1.Text, regex))
+            {
+                MessageBox.Show("Please enter a valid number separated by : ");
+                textBox1.Text = string.Empty;
+                return;
+            }
+
+            if (GetNamesFromXml(m_filepath).Contains(textBox2.Text))
+            {
+                MessageBox.Show("The name is already exists!");
+                textBox2.Text = String.Empty;
+                textBox2.Focus();
+                return;
+            }
+
+            if (!Regex.IsMatch(textBox3.Text, regex2))
+            {
+                MessageBox.Show("Please enter only a valid country shortcat");
+                textBox3.Text = string.Empty;
+                return;
+            }
+
+            xdoc.Load(m_filepath);
+
+            List<string> namesList = new List<string>();
+
+            foreach (XmlNode node in xdoc.SelectNodes("//Mappings/Descriptions/Description/Name"))
+            {
+                string name = node.InnerText;
+
+                namesList.Add(name);
+            }
+
+            xdoc.Load(m_filepath);
+            XmlNode descriptionsNode = xdoc.SelectSingleNode("//Mappings/Descriptions");
+            XmlNode description = xdoc.CreateElement("Description");
+            descriptionsNode.SelectSingleNode("//Mappings/Descriptions/").AppendChild(description);
+            XmlNode Name = xdoc.CreateElement("Name");
+            Name.InnerText = textBox2.Text;
+            description.AppendChild(Name);
+            XmlNode dis = xdoc.CreateElement("DIS");
+            dis.InnerText = textBox1.Text;
+            description.AppendChild(dis);
+            XmlNode battleDimension = xdoc.CreateElement("BattleDimension");
+            battleDimension.InnerText = comboBox1.SelectedItem.ToString();
+            description.AppendChild(battleDimension);
+            XmlNode country = xdoc.CreateElement("Country");
+            country.InnerText = textBox3.Text;
+            description.AppendChild(country);
+            XmlNode function = xdoc.CreateElement("Function");
+            function.InnerText = textBox4.Text;
+            description.AppendChild(function);
+            XmlNode platformId = xdoc.CreateElement("platformId");
+            description.AppendChild(platformId);
+            xdoc.DocumentElement.AppendChild(description);
+            xdoc.Save(m_filepath);
+            MessageBox.Show("Entity added succefully");
+
+
+
+            //return namesList.Distinct().ToList();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+
+            m_filepath = openFileDialog1.FileName;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+         
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(@"m_filepath");
+            dataGridView1.DataSource = dataSet.Tables[0];
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
